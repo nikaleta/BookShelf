8a5b428f28ded5f235ec10cb18eff374a014303c
V2 <alexandrualexandru.gheorghita@yopeso.com>
2014-10-23 15:55:35 +0300

    books navigation

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,51 @@
+class BooksController < ApplicationController
+
+  def new
+    @book = Book.new
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+
+    if @book.update (book_params)
+      redirect_to @book
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+
+    redirect_to books_path
+  end
+
+  def create
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to @book
+    else
+      render 'new'
+    end
+  end
+
+  private
+   def book_params
+    params.require(:book).permit(:title, :author, :description, :publication)
+   end
+
+end
